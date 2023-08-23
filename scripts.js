777fee67272b9ed60e04635e60a5857798ec3927
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-08-23 23:24:51 +0200

    stylecommit

--- scripts.js
+++ scripts.js
@@ -3,12 +3,18 @@ const amarelo = document.getElementById("amarelo");
 const verde = document.getElementById("verde");
 const vermelhoP = document.getElementById("vermelhoP");
 const verdeP = document.getElementById("verdeP");
+const amarelo2 = document.getElementById("amarelo2");
+const vermelho2 = document.getElementById("vermelho2");
+const verde2 = document.getElementById("verde2");
+const vermelhoP2 = document.getElementById("vermelhoP2");
+const verdeP2 = document.getElementById("verdeP2");
+
 
 const estados = [
-    { cor: "vermelho", duracao: 10000 }, 
-    { cor: "amarelo", duracao: 3000 },   
-    { cor: "verde", duracao: 10000 },  
-    { cor: "amarelo", duracao: 3000 },     
+    { cor: "vermelho", duracao: 7000 },  
+    { cor: "amarelo2", duracao: 1000 }, 
+    { cor: "verde", duracao: 7000 },  
+    { cor: "amarelo", duracao: 1000 },   
 ];
 
 
@@ -21,21 +27,28 @@ function alternarCores() {
     if (estado.cor === "vermelho") {
         vermelho.style.backgroundColor = "rgb(255, 0, 0)";
         verdeP.style.backgroundColor = "rgb(4, 255, 4)";
-      
+        vermelhoP2.style.backgroundColor = "rgb(255, 0, 0)";
+        verde2.style.backgroundColor = "rgb(4, 255, 4)";
+        vermelho2.style.backgroundColor = "";
+        vermelhoP.style.backgroundColor = "";
     } else {
-        vermelho.style.backgroundColor = "";
         verdeP.style.backgroundColor = "";
+        verde2.style.backgroundColor = "";
+       
     }
-    
-    amarelo.style.backgroundColor = estado.cor === "amarelo" ? "rgb(255, 255, 12)" : "";
-    
+
+    amarelo2.style.backgroundColor = estado.cor === "amarelo2" ? "rgb(255, 255, 12)" : "";
+
     if (estado.cor === "verde") {
+        vermelho.style.backgroundColor = "";
         vermelhoP.style.backgroundColor = "rgb(255, 0, 0)";
         verde.style.backgroundColor = "rgb(4, 255, 4)";
-        
-    } else {
+        verdeP2.style.backgroundColor = "rgb(4, 255, 4)";
+        vermelho2.style.backgroundColor = "rgb(255, 0, 0)";
+        vermelhoP2.style.backgroundColor = "";
+    } else {  
         verde.style.backgroundColor = "";
-        vermelhoP.style.backgroundColor = "";
+        verdeP2.style.backgroundColor = "";
     }
 
     amarelo.style.backgroundColor = estado.cor === "amarelo" ? "rgb(255, 255, 12)" : "";
